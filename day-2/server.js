742ed62324d1dd808a459a44cdf5efe38b5b6b7e
zaidalshreef <zaidalshreef@gmail.com>
2022-08-29 15:14:17 +0300

    day-2

---
+++ day-2/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => 
+{
+    res.send('Welcome to My first homework with node.js')
+}
+    )
+
+app.get('/user')
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
